06f3207e819892aa7625d92ceec8d5e6ff7f8184
SergeyKalutsky <skalutsky@gmail.com>
2022-12-18 11:34:54 +0300

    changed imports order

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import cv2
+import numpy as np
 from mss import mss
 from time import sleep
-import numpy as np
 from image_seg import is_right
-import cv2
 from controlls import start_fishing
+
 sleep(1)
 with mss() as sct:
 
